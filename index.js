abfba23d6472fdbbce11c29e75963e57b885295f
Edris_Azimi <Edris.azimi.pro@gmail.com>
2021-12-13 11:08:14 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
-// Write your code here
+  
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2 ;
+
+
+   let random = Math.ceil(Math.random() * 10);
+
+
+
+
+  let num3 = 8
+  let num4 = 2
+
+  const mod = num3 / num4;
+
+
+  const max= Math.max(1, 2, 3, 4, 20);
+
+
